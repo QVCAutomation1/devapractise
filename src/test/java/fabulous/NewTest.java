5ba1746e40b9a4f05d2706890c01bec604d77e5e
QVCAutomation1 <40177440+QVCAutomation1@users.noreply.github.com>
2020-06-13 20:34:33 +0530

    Update NewTest.java
    chenged the name

--- src/test/java/fabulous/NewTest.java
+++ src/test/java/fabulous/NewTest.java
@@ -33,7 +33,7 @@ public class NewTest {
 
   @AfterMethod
   public void afterMethod() {
-	  System.out.println("after the  before the test annotations method");
+	  System.out.println("after the  before test annotations method");
   }
 
   @BeforeClass
